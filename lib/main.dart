808436787af9ba91c1b39417bd9da33bf6f58120
nano-rayhan <rr5345933@gmail.com>
2025-01-29 00:11:48 +0600

    Database use

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:my_apps/data/local/db_helper.dart';
 import 'package:my_apps/introPage.dart';
 import 'package:my_apps/splashScreen.dart';
 import 'package:my_apps/widgets/rounde_btn.dart';
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  splashscreen(),
+      home: splashscreen(),
     );
   }
 }
@@ -39,19 +40,35 @@ class MyHomePage extends StatefulWidget{
 class MyHomeState extends State<MyHomePage>{
   var wt = TextEditingController();
   var count = 0;
-  
+  //DBHelper  db = DBHelper();
   RangeValues values = RangeValues(0, 1);
   var ft = TextEditingController();
   var inch = TextEditingController();
   var result = "";
   var bgColo = Colors.black38;
+
+   List<Map<String, dynamic>> allNotes = [];
+
+   DBHelper? dbRef;
+  void initState() {
+    super.initState();
+    dbRef = DBHelper.getInstance;
+    getNotes();
+  }
+
+  void getNotes() async{
+    allNotes = await dbRef!.getAllNotes();
+    setState(() {
+      
+    });
+  }
   @override
   Widget build(BuildContext context) {
     
     RangeLabels labels = RangeLabels(values.start.toString(), values.end.toString()); 
     return Scaffold (
       appBar: AppBar(
-        title: Text("Be Healthy"),
+        title: Text("Note"),
         backgroundColor: Colors.amber,
         actions: [
           Stack(children: [
@@ -65,88 +82,25 @@ class MyHomeState extends State<MyHomePage>{
       ),
 
 
-    body: Container( 
-      color: bgColo,
-      child: Center(
-      
-      child: Container(
-      width: 300,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text("BMI", style: TextStyle(fontSize: 30),),
-        TextField(
-          controller: wt,
-          decoration: InputDecoration(
-            label: Text('Enter yout weight'),
-            prefixIcon: Icon(Icons.line_weight),
-          ),
-          keyboardType: TextInputType.number,
-        ),
-        SizedBox(height: 20,),
-        TextField(
-          controller: ft,
-          decoration: InputDecoration(
-            label: Text('Enter yout height(in feat)'),
-            prefixIcon: Icon(Icons.line_weight),
-          ),
-          keyboardType: TextInputType.number,
-        ),
-        SizedBox(height: 20,),
-        TextField(
-          controller: inch,
-          decoration: InputDecoration(
-            label: Text('Enter yout height(inches)'),
-            prefixIcon: Icon(Icons.line_weight),
-          ),
-          keyboardType: TextInputType.number,
-        ),
-        SizedBox(height:  20,),
-        ElevatedButton(onPressed: (){
-          var w = wt.text.toString();
-          var f= ft.text.toString();
-          var i = inch.text.toString();
-
-          if(w != null && f!= null && i != null){
-            var iw = int.parse(w);
-            var ih = int.parse(f);
-            var ii = int.parse(i);
-
-            var tinch  = (ih*12) + ii;
-            var tcm = tinch*2.54;
-            var tm = tcm/100;
-
-            var bmi = iw/(tm*tm);
-            var msg = "";
-            if(bmi>25){
-              msg = "You are OverWeight!";
-              bgColo = Colors.red;
-            }
-            else if(bmi<18){
-              msg = "You are underWeight!";
-              bgColo = Colors.blue;
-            }
-            else{
-              msg = "you are Healthy!";
-              bgColo = Colors.green;
-            }
-
-           setState(() {
-              result = "$msg \n Your BMI is: ${bmi.toStringAsFixed(2)}";
-           });
-          }
-          else{
-            setState(() {
-              result = "Please fill all the ";
-            });
-          }
-        }, child: Text("Calculate")),
-        SizedBox(height: 20,),
-        Text(result, style: TextStyle(fontSize: 30),)
-    ],),
-    ) 
-    )
-    )
+    body: allNotes.isNotEmpty ? ListView.builder( 
+      itemCount: allNotes.length,
+      itemBuilder:(_, index){
+        return ListTile(
+          leading: Text("${allNotes[index][DBHelper.COLUMN_NOTE]}"),
+          title: Text(allNotes[index][DBHelper.COLUMN_TITLE]),
+          subtitle: Text(allNotes[index][DBHelper.COLUMN_DEC]),
+        );
+    } ) :Center(
+      child: Text("No note yet"),
+    ),
+     
+     floatingActionButton: FloatingActionButton(
+      onPressed: () async{
+      bool check = await dbRef!.addNote(mTitle: "Myself", mDesc: "My name is rayhan.");
+      If(check){
+        getNotes(); 
+      }
+     },child: Icon(Icons.add),),
     );
   }
 }
